776cfc70ed9215d1170c7d218acce19cf7caab40
Victor Juliani <juliani_rodrigues@hotmail.com>
2021-07-09 13:20:59 -0300

    Fixed path change.

--- pfg-21-22/src/test/java/StepDefinitions/TestRunner.java
+++ pfg-21-22/src/test/java/StepDefinitions/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features=
-	"C:\\Users\\Victor\\git_projects\\MC030\\eclipse_workspace\\CucumberJava\\src\\test\\resources\\Features", glue= {"StepDefinitions"} 	)
+	"C:\\Users\\Victor\\git_projects\\MC030\\eclipse_workspace\\pfg-21-22\\src\\test\\resources\\Features", glue= {"StepDefinitions"} 	)
 public class TestRunner {
 
 }
